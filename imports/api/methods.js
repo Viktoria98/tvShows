0f15cca0b5a05cc950c72ff1ce065712a75da38c
Vika <krivosheevaviktoria98@gmail.com>
2019-07-01 12:27:11 +0300

    heroku

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -3,10 +3,17 @@ import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
 import { Films } from './db/filmsdb';
 
-const { settings } = Meteor;
-const {
-  url, traktApiKey, tmdbUrl, tmdbApiKey, imgUrl,
-} = settings;
+// const { settings } = Meteor;
+// const {
+//   url, traktApiKey, tmdbUrl, tmdbApiKey, imgUrl,
+// } = settings;
+
+const url = 'https://api.trakt.tv/movies/popular';
+const traktApiKey = '9ac61bf7e1b7fff10d7fdb05559dbbeaa671b5bc1d14cc8e69a385b422dcdf4d';
+const tmdbApiKey = '2ed8048b26045f118396d8a886f19401';
+const tmdbUrl = 'https://api.themoviedb.org/3/movie';
+const imgUrl = 'https://image.tmdb.org/t/p/w200';
+
 
 const myHeaders = {
   'Content-Type': 'application/json',
